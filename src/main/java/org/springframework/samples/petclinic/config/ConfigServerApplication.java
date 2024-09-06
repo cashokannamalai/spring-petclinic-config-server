913da8828cbbb82e808bb75181853eb50dc11dab
cashokannamalai <166033475+cashokannamalai@users.noreply.github.com>
2024-09-06 16:10:28 +0530

    Update ConfigServerApplication.java

--- src/main/java/org/springframework/samples/petclinic/config/ConfigServerApplication.java
+++ src/main/java/org/springframework/samples/petclinic/config/ConfigServerApplication.java
@@ -1,4 +1,20 @@
+/*
+ * Copyright 2002-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.samples.petclinic.config;
+
 
 import io.rollout.rox.server.Rox;
 import org.slf4j.Logger;
@@ -6,39 +22,33 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.annotation.Autowired;
-import javax.annotation.PostConstruct;
-
 
 import java.util.concurrent.ExecutionException;
 
-@SpringBootApplication
+
+/**
+ * @author Maciej Szarlinski
+ */
 @EnableConfigServer
+@SpringBootApplication
 public class ConfigServerApplication {
 
-    private static final Logger log = LoggerFactory.getLogger(ConfigServerApplication.class);
 
-    @Value("${feature.management.key}")
-    private String featureManagementKey;
+	private static final Logger log = LoggerFactory.getLogger(ConfigServerApplication.class);
 
-    @Autowired
-    private Flags flags;
+	public static void main(String[] args) throws ExecutionException, InterruptedException {
+		SpringApplication.run(ConfigServerApplication.class, args);
+		Flags flags = new Flags();
 
-    public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class, args);
-    }
+		// Register the flags container under a namespace
+		Rox.register("default", flags);
 
-    @PostConstruct
-    public void initializeFeatureFlags() throws ExecutionException, InterruptedException {
-        // Register the flags container under a namespace
-        Rox.register("default", flags);
+		// Setup connection with the feature management environment key
+		Rox.setup("64595968-4a49-4f49-7ebd-b08938ad1d2e").get();
 
-        // Setup connection with the feature management environment key
-        Rox.setup(featureManagementKey).get();
+		// Check and print the value of the 'enableTutorial' flag
+		boolean isTutorialEnabled = flags.enableTutorial.isEnabled();
+		log.info("enableTutorial value is {}", isTutorialEnabled ? "true" : "false");
 
-        // Check and print the value of the 'enableTutorial' flag
-        boolean isTutorialEnabled = flags.enableTutorial.isEnabled();
-        log.info("enableTutorial value is {}", isTutorialEnabled ? "true" : "false");
-    }
+	}
 }
