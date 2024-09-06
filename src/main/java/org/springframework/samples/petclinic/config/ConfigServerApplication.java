58bb4d2b7b7f4816167d56d1e2c4cbeae32e3cc3
cashokannamalai <166033475+cashokannamalai@users.noreply.github.com>
2024-09-06 15:41:54 +0530

    Update ConfigServerApplication.java

--- src/main/java/org/springframework/samples/petclinic/config/ConfigServerApplication.java
+++ src/main/java/org/springframework/samples/petclinic/config/ConfigServerApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import javax.annotation.PostConstruct;
+
 
 import java.util.concurrent.ExecutionException;
 
