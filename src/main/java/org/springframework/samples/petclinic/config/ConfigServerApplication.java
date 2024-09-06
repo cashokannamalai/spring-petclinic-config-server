7822cba5a41e335264b2623013fd8a34cdf7784b
cashokannamalai <166033475+cashokannamalai@users.noreply.github.com>
2024-09-06 13:06:33 +0530

    Update ConfigServerApplication.java

--- src/main/java/org/springframework/samples/petclinic/config/ConfigServerApplication.java
+++ src/main/java/org/springframework/samples/petclinic/config/ConfigServerApplication.java
@@ -1,33 +1,54 @@
+/*
+ * Copyright 2002-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.samples.petclinic.config;
 
+
+import io.rollout.rox.server.Rox;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import io.rollout.rox.server.Rox;
-import io.rollout.rox.server.RoxClient;
-import io.rollout.rox.server.RoxFlag;
-import io.rollout.rox.server.RoxFlags;
 
-@SpringBootApplication
+import java.util.concurrent.ExecutionException;
+
+
+/**
+ * @author Maciej Szarlinski
+ */
 @EnableConfigServer
+@SpringBootApplication
 public class ConfigServerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class, args);
 
-        try {
-            // Initialize Rox client
-            RoxClient roxClient = Rox.setup("7d77f2ce-ada9-4276-564c-ac004dc6c37e").get();
+	private static final Logger log = LoggerFactory.getLogger(ConfigServerApplication.class);
 
-            // Access flags
-            RoxFlags flags = roxClient.getFlags();
+	public static void main(String[] args) throws ExecutionException, InterruptedException {
+		SpringApplication.run(ConfigServerApplication.class, args);
+		Flags flags = new Flags();
 
-            // Check and print the value of the enableTutorial flag
-            boolean enableTutorial = flags.get("enableTutorial").isEnabled();
-            System.out.printf("enableTutorial value is %s%n", enableTutorial ? "true" : "false");
+		// Register the flags container under a namespace
+		Rox.register("default", flags);
 
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        }
-    }
+		// Setup connection with the feature management environment key
+		Rox.setup("7d77f2ce-ada9-4276-564c-ac004dc6c37e").get();
+
+		// Check and print the value of the 'enableTutorial' flag
+		boolean isTutorialEnabled = flags.enableTutorial.isEnabled();
+		log.info("enableTutorial value is {}", isTutorialEnabled ? "true" : "false");
+
+	}
 }
