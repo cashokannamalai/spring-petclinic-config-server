6f55fe8cb4bd2c4538a72e87610327b0fdff1c62
cashokannamalai <166033475+cashokannamalai@users.noreply.github.com>
2024-09-11 15:51:37 +0530

    Delete src/main/java/org/springframework/samples/petclinic/config/Flags.java

--- src/main/java/org/springframework/samples/petclinic/config/Flags.java
+++
@@ -1,8 +0,0 @@
-package org.springframework.samples.petclinic.config;
-
-import io.rollout.configuration.RoxContainer;
-import io.rollout.flags.RoxFlag;
-
-public class Flags implements RoxContainer {
-    public RoxFlag enableTutorial = new RoxFlag();
-}
